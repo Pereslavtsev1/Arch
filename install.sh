61a1c52b3bc3ed798069af26ab30574ae0ea5881
Pereslavtsev Vladimir <vovapereslavtsev@gmail.com>
2025-09-07 13:15:29 +0300

    feat: Enhance install.sh script with new tools and improved error handling

--- install.sh
+++ install.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-# Set -e to exit script on error
 set -e
 
-# Function to install additional tools
 install_tools() {
-    tools=("git" "curl" "neovim" "btop" "nitch" "vesktop-bin" "amneziavpn-bin" "zoxide" "jq" "xh" "fd" "fzf" "nvidia" "nvidia-utils" "nvibrant-bin" "nautilus" "yazi" "zen-browser-bin" "obsidian" "autoconf" "automake" "pavucontrol" "ripgrep")
+    tools=(
+        git curl neovim btop nitch vesktop-bin amneziavpn-bin zoxide jq
+        xh fd fzf nvidia nvidia-utils
+        nvibrant-bin nautilus yazi zen-browser-bin obsidian autoconf automake pavucontrol ripgrep eza unzip
+        calf easyeffects
+        noto-fonts-emoji matugen
+    )
 
     echo "Installing additional tools..."
     for tool in "${tools[@]}"; do
-        if ! pacman -Qs "$tool" >/dev/null; then
-            echo "Installing $tool..."
-            yay -S --noconfirm --needed "$tool"
-        else
-            echo "$tool is already installed"
+        if ! yay -S --noconfirm --needed "$tool"; then
+            echo "⚠️ Failed to install $tool, skipping..."
         fi
     done
 }
 
-# Main script execution
 echo "Starting installation script..."
-
 install_tools
-
 echo "All tools have been installed successfully."
